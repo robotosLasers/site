afd031330635ebda437c45d23f8e0085b802b6a6
jose antonio <ellocodelgorro@gmail.com>
2014-10-13 21:19:56 +0200

    web 2.0

--- js/init.js
+++ js/init.js
@@ -1,62 +1,160 @@
 /*
-	Aerial by HTML5 UP
+	Helios by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function() {
+(function($) {
 
+	var settings = {
+		
+		// Header (homepage only)
+			header: {
+				fullScreen: true,
+				fadeIn: true,
+				fadeDelay: 500
+			},
+
+		// Carousels
+			carousels: {
+				speed: 4,
+				fadeIn: true,
+				fadeDelay: 250
+			},
+	
+	};
+	
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			'global': { range: '*', href: 'css/style.css', viewport: { scalable: false } },
-			'wide': { range: '-1680', href: 'css/style-wide.css' },
-			'normal': { range: '-1280', href: 'css/style-normal.css' },
-			'mobile': { range: '-736', href: 'css/style-mobile.css' },
-			'mobilep': { range: '-480', href: 'css/style-mobilep.css' }
+			'global':	{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 48 } },
+			'wide':		{ range: '-1680', href: 'css/style-wide.css', containers: 1200 },
+			'normal':	{ range: '-1280', href: 'css/style-normal.css', containers: '100%', grid: { gutters: 36 } },
+			'narrow':	{ range: '-960', href: 'css/style-narrow.css', grid: { gutters: 32 } },
+			'narrower': { range: '-840', href: 'css/style-narrower.css', grid: { collapse: true } },
+			'mobile':	{ range: '-736', href: 'css/style-mobile.css', grid: { gutters: 16 }, viewport: { scalable: false } }
+		},
+		plugins: {
+			layers: {
+				config: {
+					transformTest: function() { return skel.vars.isMobile; }
+				},
+				navPanel: {
+					hidden: true,
+					breakpoints: 'mobile',
+					position: 'top-left',
+					side: 'top',
+					width: '100%',
+					height: 250,
+					animation: 'pushY',
+					clickToHide: true,
+					swipeToHide: false,
+					html: '<div data-action="navList" data-args="nav"></div>',
+					orientation: 'vertical'
+				},
+				navButton: {
+					breakpoints: 'mobile',
+					position: 'top-center',
+					side: 'top',
+					width: 100,
+					height: 50,
+					html: '<div class="toggle" data-action="toggleLayer" data-args="navPanel"></div>'
+				}
+			}
 		}
 	});
 
-	// Events (JS).
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header =  $('#header');
+			
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+			
+			$window.on('load', function() {
+				setTimeOut(function(){$body.removeClass('is-loading');}, 3000);
+			});
+			
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Forms (IE<10).
+			var $form = $('form');
+			if ($form.length > 0) {
+				
+				$form.find('.form-button-submit')
+					.on('click', function() {
+						$(this).parents('form').submit();
+						return false;
+					});
 		
-		// Remove "loading" class once the page has fully loaded.
-			window.onload = function() {
-				document.body.className = '';
+				if (skel.vars.IEVersion < 10) {
+					$.fn.n33_formerize=function(){var _fakes=new Array(),_form = $(this);_form.find('input[type=text],textarea').each(function() { var e = $(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = $(this); var x = $($('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = $(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = $(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { $(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); $(this).find('select').val($('option:first').val()); $(this).find('input,textarea').each(function() { var e = $(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
+					$form.n33_formerize();
+				}
+
 			}
 
-		// Prevent scrolling on touch.
-			window.ontouchmove = function() {
-				return false;
+		// Dropdowns.
+			$('#nav > ul').dropotron({ 
+				mode: 'fade',
+				speed: 350,
+				noOpenerFade: true,
+				alignment: 'center'
+			});
+
+		// Scrolly links.
+			$('.scrolly').scrolly();			
+
+		// Carousels.
+		
+		// Header.
+			if ($body.hasClass('homepage')) {
+				
+				if (settings.header.fullScreen) {
+					
+					$window.bind('resize.helios', function() {
+						window.setTimeout(function() {
+							var s = $header.children('.inner');
+							var sh = s.outerHeight(), hh = $window.height(), h = Math.ceil((hh - sh) / 2) + 1;
+
+							$header
+								.css('padding-top', h)
+								.css('padding-bottom', h);
+						}, 0);
+					}).trigger('resize');
+				
+				}
+
+				if (settings.header.fadeIn) {
+
+					$.n33_preloadImage = function(url, onload) { var $img = $('<img />'), _IEVersion = (navigator.userAgent.match(/MSIE ([0-9]+)\./) ? parseInt(RegExp.$1) : 99); $img.attr('src', url); if ($img.get(0).complete || _IEVersion < 9) (onload)(); else $img.load(onload); };
+					
+					$('<div class="overlay" />').appendTo($header);
+					
+					$window
+						.load(function() {
+							var imageURL = $header.css('background-image').replace(/"/g,"").replace(/url\(|\)$/ig, "");
+
+							$.n33_preloadImage(imageURL, function() {
+								
+								if (skel.vars.IEVersion < 10)
+									$header.children('.overlay').fadeOut(2000);
+								else
+									window.setTimeout(function() {
+										$header.addClass('ready');
+									}, settings.header.fadeDelay);
+							
+							});
+						});
+				
+				}
+
 			}
 
-		// Fix scroll position on orientation change.
-			window.onorientationchange = function() {
-				document.body.scrollTop = 0;
-			}
+	});
 
-	/*
-
-	// Events (jQuery).
-	// Aerial doesn't need jQuery, but if you're going to use it anyway remove the
-	// block of JS events above and use the jQuery-based ones below instead.
-
-		jQuery(window)
-
-			// Remove "loading" class once the page has fully loaded.
-				.on('load', function() {
-					jQuery('body').removeClass('loading');
-				})
-
-			// Prevent scrolling on touch.
-				.on('touchmove', function() {
-					return false;
-				})
-
-			// Fix scroll position on orientation change.
-				.on('orientationchange', function() {
-					jQuery('body').scrollTop(0);
-				});
-
-	*/
-
-})();
+})(jQuery);
